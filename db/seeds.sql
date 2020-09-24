3a605d65e6b8d505715d01d5b0ddd5896c4637c1
Garrett Dobson <garrettdobson33@gmail.com>
2020-09-23 22:12:33 -0400

    sql files are updated and complete

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Carolina Classic Burger');
+INSERT INTO burgers (burger_namename) VALUES ('Pimento Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Cheeseburger', true);
